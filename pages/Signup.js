63b019d52ffdd4d2ec21791fbb9f711489dd77a1
moussa23 <mosawi.man@live.fr>
2020-05-17 00:37:25 +0000

    test

--- pages/Signup.js
+++ pages/Signup.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 const Signup = () => {
     return (
         <View style={styles.container} >
-            <Text> iscreption </Text>
+            <Text> todo</Text>
         </View>
     )
 }
